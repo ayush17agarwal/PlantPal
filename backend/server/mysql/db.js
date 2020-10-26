cc2464e5ff475a062f3a8f3ab35d04f7331cfddf
Sabelle Huang <sabelle2@illinois.edu>
2020-10-26 02:14:25 -0700

    add some test screens

--- backend/server/mysql/db.js
+++ backend/server/mysql/db.js
@@ -1,18 +1,18 @@
-const mysql = require('mysql');
+// const mysql = require('mysql');
 
-const connection = mysql.createConnection({
-  host: 'localhost',
-  port: '3306',
-  user: 'root',
-  database: 'plantpal',
-  password: 'covid-19',
-});
+// const connection = mysql.createConnection({
+//   host: 'localhost',
+//   port: '3306',
+//   user: 'root',
+//   database: 'plantpal',
+//   password: 'covid-19',
+// });
 
-connection.connect((err) => {
-  if (err) {
-    throw err;
-  }
-  console.log('MySQL Connected Successfully!');
-});
+// connection.connect((err) => {
+//   if (err) {
+//     throw err;
+//   }
+//   console.log('MySQL Connected Successfully!');
+// });
 
-module.exports = connection;
+// module.exports = connection;
