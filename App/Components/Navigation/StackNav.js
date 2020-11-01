28b80130dafcbb6ac4a89d07700a84704e647a59
Sabelle Huang <sabelle2@illinois.edu>
2020-10-31 21:16:32 -0700

    progess

--- App/Components/Navigation/StackNav.js
+++ App/Components/Navigation/StackNav.js
@@ -1,6 +1,7 @@
 import React from "react";
-import { TouchableOpacity, Image, StyleSheet, Button, Icon } from "react-native";
+import { TouchableOpacity, Image, StyleSheet, Button, Text, View} from "react-native";
 import { createStackNavigator } from "@react-navigation/stack";
+// import MenuButton from 'react-native-menu-button'
 
 import Social from '../Social';
 import Garden from "../Garden";
@@ -19,10 +20,44 @@ const screenOptionStyle = {
     headerBackTitle: " ",
 };
 
+// const DropGardenMenu = ({ navigation }) => {
+//     menuGroup= [
+//         {key:"0",value:"menu1",text:"menu1"},
+//         {key:"1",value:"menu2",text:"menu2"},
+//         {key:"2",value:"菜单3",text:"菜单3"},
+//         {key:"3",value:"菜单4",text:"菜单4"},
+//       ]
+//     return (
+//         <View>
+//         <View style={styles.top}>
+//           <MenuButton  
+//             buttonStyle={[styles.rightButton]} 
+//             menuGroup={menuGroup}
+//             onSelect={this._handleOnSelect.bind(this)} 
+//             optionSelectedStyle={{backgroundColor:"red"}}
+//           />
+//         </View>
+//         <Text style={styles.text}>{`select ${this.state.selectData}`}</Text>
+//       </View>
+//     );
+// }
+
 const GardenStackNav = () => {
     return (
         <Stack.Navigator screenOptions={screenOptionStyle}>
-            <Stack.Screen name="Garden" component={Garden} />
+            <Stack.Screen 
+                name="garden" 
+                component={Garden}
+                options={({ navigation }) => ({
+                    headerRight: () => (
+                        // <TouchableOpacity onPress={this.DropGardenMenu} >
+                        <TouchableOpacity onPress={() => navigation.navigate('settings')} >
+                            <Image 
+                                style={styles.icons} 
+                                source={require('../../Assets/down-arrow.png')} />
+                        </TouchableOpacity>
+                    )
+                })} />
             <Stack.Screen name="Plant" component={Plant} />
         </Stack.Navigator>
     );
@@ -31,7 +66,7 @@ const GardenStackNav = () => {
 const SocialStackNav = () => {
     return (
       <Stack.Navigator screenOptions={screenOptionStyle}>
-            <Stack.Screen name="Social" component={Social} />
+            <Stack.Screen name="social" component={Social} />
       </Stack.Navigator>
     );
 }
@@ -39,7 +74,7 @@ const SocialStackNav = () => {
 const SearchStackNav = () => {
     return (
       <Stack.Navigator screenOptions={screenOptionStyle}>
-            <Stack.Screen name="Search" component={Search} />
+            <Stack.Screen name="search" component={Search} />
       </Stack.Navigator>
     );
 }
@@ -48,27 +83,49 @@ const UserStackNav = () => {
     return (
       <Stack.Navigator screenOptions={screenOptionStyle}>
             <Stack.Screen 
-                name="User" 
+                name="user" 
                 component={User}
                 options={({ navigation }) => ({
                     headerRight: () => (
-                        <TouchableOpacity onPress={() => navigation.navigate('Settings')} >
+                        <TouchableOpacity onPress={() => navigation.navigate('settings')} >
                             <Image 
-                                style={styles.settingsButton} 
-                                source={require('../Assets/settings.png')} />
+                                style={styles.icons} 
+                                source={require('../../Assets/settings.png')} />
                         </TouchableOpacity>
                     )
-                })} 
-            />
-            <Stack.Screen name="Settings" component={Settings} />
+                })} />
+            <Stack.Screen name="settings" component={Settings} />
       </Stack.Navigator>
     );
 }
 
 const styles = StyleSheet.create({
-    settingsButton: {
+    icons: {
         width: 25,
         height: 25,
-    }
+        right: 10,
+    },
+    top:{
+        backgroundColor: '#FFFFFF',
+        paddingTop: 20,
+        top: 0,
+        height: 64,
+        right: 0,
+        left: 0,
+        borderBottomWidth: 0.5,
+        borderBottomColor: '#828287',
+        position: 'relative',
+      },
+      text:{
+        marginTop:20,
+      },
+      rightButton: {
+        width: 100,
+        height: 37,
+        position: 'absolute',
+        bottom: 8,
+        right: 2,
+        padding: 8
+      },
 });
 export { GardenStackNav, SocialStackNav, SearchStackNav, UserStackNav };
--- App/Components/Navigation/TabNav.js
+++ App/Components/Navigation/TabNav.js
@@ -8,51 +8,51 @@ const Tab = createBottomTabNavigator();
 const TabNav = () => {
   return (
     <Tab.Navigator
-      initialRouteName="Garden"
+      initialRouteName="garden"
       tabBarOptions={{
         style: { backgroundColor: '#769CB9'},
         activeTintColor: '#C8E3F9',
         inactiveTintColor: '#FFFFFF' }} >
       <Tab.Screen
-        name="Social"
+        name="social"
         component={SocialStackNav}
         options={{
-          tabBarLabel: 'Social',
+          tabBarLabel: 'social',
           tabBarIcon: ({ focused }) => {
             const image = focused
-            ? require('../Assets/globe_sky.png')
-            : require('../Assets/globe.png')
+            ? require('../../Assets/globe_sky.png')
+            : require('../../Assets/globe.png')
             return (
               <Image source={image} style={{height:25, width:25}}/> )}}} />
       <Tab.Screen
-        name="Garden"
+        name="garden"
         component={GardenStackNav}
         options={{
-          tabBarLabel: 'Garden',
+          tabBarLabel: 'garden',
           tabBarIcon: ({ focused }) => {
             const image = focused
-            ? require('../Assets/leaf_sky.png')
-            : require('../Assets/leaf.png')
+            ? require('../../Assets/leaf_sky.png')
+            : require('../../Assets/leaf.png')
             return ( <Image source={image} style={{height:25, width:25}}/> )}}} />
       <Tab.Screen
-        name="Search"
+        name="search"
         component={SearchStackNav}
         options={{
-          tabBarLabel: 'Search',
+          tabBarLabel: 'search',
           tabBarIcon: ({ focused }) => {
             const image = focused
-            ? require('../Assets/loupe_sky.png')
-            : require('../Assets/loupe.png')
+            ? require('../../Assets/loupe_sky.png')
+            : require('../../Assets/loupe.png')
             return ( <Image source={image} style={{height:25, width:25}}/> )}}} />
       <Tab.Screen
-        name="User"
+        name="user"
         component={UserStackNav}
         options={{
-          tabBarLabel: 'User',
+          tabBarLabel: 'user',
           tabBarIcon: ({ focused }) => {
             const image = focused
-            ? require('../Assets/user_sky.png')
-            : require('../Assets/user.png')
+            ? require('../../Assets/user_sky.png')
+            : require('../../Assets/user.png')
             return ( <Image source={image} style={{height:25, width:25}}/> )}}} />
     </Tab.Navigator>
   );
