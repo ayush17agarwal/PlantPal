e96e6aa1c8855f102f1fa222fe4cb9efe567723a
Sabelle Huang <sabelle2@illinois.edu>
2020-11-05 00:22:32 -0800

    remove commented code

--- App/App.js
+++ App/App.js
@@ -80,34 +80,10 @@ const App: () => React$Node = () => {
       </Stack.Navigator> */}
       <Content>
         {
-        Plants.map((plantItem) => {
-       
-          return(
-           
-            <PlantCard plant={plantItem}/>
-            
-            // <Card containerStyle={card_styles.Card}>
-            //   <Card.Title>{plant.nickname}</Card.Title>
-            //   <Card.Divider/>
-            //   <Text>Name: {plant.name}</Text>
-            //   <View>
-            //     <Image
-            //       source={'./Assets/leaf.png'}
-            //       resizeMode={'contain'} 
-            //       style={{width: 50, height: 50}}/>
-            //   </View>
-            //   <View>
-            //     <Table>
-            //       <TableWrapper>
-            //         <Row data={["date planted", plant.datePlanted]}></Row>
-            //         <Row data={["last watered", plant.lastWatered]}></Row>
-            //         <Row data={["health", plant.health]}></Row>
-            //       </TableWrapper>
-            //     </Table>
-            //   </View>
-            // </Card>
-          )
-        })
+          Plants.map((plantItem) => {
+        
+            return ( <PlantCard plant={plantItem}/> );
+          })
         }
         </Content>
     </NavigationContainer>
