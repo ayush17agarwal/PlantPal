68ac8e0155a0b56fd442b9375c83f6f6f9f63f2d
mmattikalli <meghamattikalli@gmail.com>
2020-11-05 00:17:04 -0800

    App js

--- App/App.js
+++ App/App.js
@@ -12,7 +12,7 @@
 // }
 
 
-import React from 'react';
+import React, {Component} from 'react';
 import { SafeAreaView, StyleSheet, ScrollView, View, Text, Image, StatusBar } from 'react-native';
 import { Header, LearnMoreLinks, Colors, DebugInstructions, ReloadInstructions, } from 'react-native/Libraries/NewAppScreen';
 import { NavigationContainer } from '@react-navigation/native';
@@ -21,7 +21,7 @@ import { Card, ListItem, Button, Icon } from 'react-native-elements'
 import { Container, Content, Body, Title, Tab} from 'native-base';
 import { Table, TableWrapper, Row, Rows, Col, Cols, Cell } from 'react-native-table-component';
 
-import { PlantCard } from './Components/PlantCard'
+// import { PlantCard } from './Components/PlantCard'
 import { color } from 'react-native-reanimated';
 
 
@@ -80,11 +80,12 @@ const App: () => React$Node = () => {
       </Stack.Navigator> */}
       <Content>
         {
-        Plants.map((plant) => {
+        Plants.map((plantItem) => {
+       
           return(
-            // <>
-            // {/* <PlantCard plant={plant}></PlantCard> */}
-            // </>
+           
+            <PlantCard plant={plantItem}/>
+            
             // <Card containerStyle={card_styles.Card}>
             //   <Card.Title>{plant.nickname}</Card.Title>
             //   <Card.Divider/>
@@ -114,6 +115,31 @@ const App: () => React$Node = () => {
     </>
   );
 };
+
+const PlantCard = (props) => {
+  return(
+    <Card containerStyle={card_styles.Card}>
+      <Card.Title>{props.plant.nickname}</Card.Title>
+      <Card.Divider/>
+      <Text>Name: {props.plant.name}</Text>
+      <View>
+      <Image
+          source={{uri: 'https://facebook.github.io/react/img/logo_og.png'}}
+          resizeMode={'contain'} 
+          style={{width: 50, height: 50}}/>
+      </View>
+      <View>
+      <Table>
+          <TableWrapper>
+          <Row data={["date planted", props.plant.datePlanted]}></Row>
+          <Row data={["last watered", props.plant.lastWatered]}></Row>
+          <Row data={["health", props.plant.health]}></Row>
+          </TableWrapper>
+      </Table>
+      </View>
+  </Card>
+  );
+}
 
 const card_styles = StyleSheet.create({
   Card: {
