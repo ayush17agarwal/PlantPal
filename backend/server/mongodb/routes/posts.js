da16b8f8c1cfbe9e4734076cfca9be8fc1998545
ayush17agarwal <ayushba2@illinois.edu>
2020-11-28 00:38:41 -0600

    Everything except adding a post

--- backend/server/mongodb/routes/posts.js
+++ backend/server/mongodb/routes/posts.js
@@ -1,6 +1,53 @@
-// const router = require('express').Router();
-// let Post = require('../models/post.model');
+const router = require('express').Router();
+let Post = require('../models/post.model');
+let Comment = require('../models/comment.model');
 // const multer = require('multer');
 // const upload = multer({dest: 'uploads/'});
 
 // // router.post('/', upload.single('postImage'), (req, res, next) => {});
+
+
+//add like
+router.post('/like', (req, res) => {
+    const {post_id, username} = req.body;
+
+    Post.findById(post_id)
+        .then(post => {
+            post.likes.push(username);
+            post.save()
+                .then(() => {
+                    return res.send({success: true});
+                })
+                .catch((err) => res.status(400).json('Error: ' + err));
+        })
+        .catch((err) => res.status(400).json('Error: ' + err));
+});
+
+router.get('/num-likes', (req, res) => {
+    const {post_id} = req.query;
+
+    const agg = [{'$match': {'_id': post_id}}, {'$count': {'$sum': 1}}];
+
+    Post.aggregate(agg, (err, results) => {
+        if(err) return res.status(400).json('Error: ' + err);
+
+        return res.send(results);
+    });
+});
+
+router.get('/num-posts', (req, res) => {
+    const {username} = req.query;
+
+    const agg = [{'$match': {'username': username}}, {'$count': {'$sum': 1}}];
+
+    Post.aggregate(agg, (err, results) => {
+        if(err) return res.status(400).json('Error: ' + err);
+
+        return res.send(results);
+    });
+});
+
+
+
+
+module.exports = router;
