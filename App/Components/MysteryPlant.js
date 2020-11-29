33389da6025fe9c94920e7ca2391f29d79c23633
Sabelle Huang <sabelle2@illinois.edu>
2020-11-28 18:09:20 -0800

    search screens added

---
+++ App/Components/MysteryPlant.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { View, Text, StyleSheet, ScrollView, Image } from 'react-native';
+
+class MysteryPlant extends Component {
+    render() {
+      return(
+        <View>
+          <Text>Mystery Plant Screen</Text>
+        </View>
+      )
+    }
+
+}
+
+export default MysteryPlant;
---
+++ App/Components/PlantRelationship.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { View, Text, StyleSheet, ScrollView, Image } from 'react-native';
+
+class PlantRelationship extends Component {
+    render() {
+      return(
+        <View>
+          <Text>Plant Relationship Screen</Text>
+        </View>
+      );
+    }
+
+}
+
+export default PlantRelationship;
