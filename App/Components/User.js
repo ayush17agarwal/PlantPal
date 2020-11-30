aa0c0fe20477dda61191da6524297cf5f30b9e9c
mmattikalli <meghamattikalli@gmail.com>
2020-11-30 12:16:49 -0800

    Changes to user page

--- App/Components/User.js
+++ App/Components/User.js
@@ -1,21 +1,49 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet} from 'react-native';
+import { View, Text, StyleSheet, ScrollView} from 'react-native';
 import Settings from './Settings';
 
-// handleClick = ({ navigation }) => {
-//     navigation.navigate('Settings')
-// }
+import SubmitButton from './SubmitButton';
 
-const User = ({navigation}) => {
-    return (
-            <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text>User Screen</Text>
-            </View>
-     );
+class User extends Component {
+    state = {
+        firstname: 'ayush',
+        username: '@' + 'aagarwal2'
+    }
+
+    render(){
+        return (
+            <ScrollView style={styles.container}> 
+                <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+                    <Text style={styles.textHeader}>{this.state.firstname}</Text>
+                    <Text style={styles.textSubheader}>{this.state.username}</Text>
+                </View>
+                <View style={styles.dividerStyle} />
+            </ScrollView>
+        );
+    }
 }
+
 const styles = StyleSheet.create({
     container: {
-        flex: 1
+        flex: 1,
+        padding: 24
+    },
+    textHeader: {
+        color: '#86B58F',
+        fontSize: 35,
+        fontFamily: 'Roboto',
+        padding: 12
+    },
+    dividerStyle: {
+        borderBottomColor: '#86B58F',
+        borderBottomWidth: 2,
+        padding: 11
+    },
+    textSubheader: {
+        color: '#86B58F',
+        fontSize: 20,
+        fontFamily: 'Roboto'
     }
 });
+
 export default User;
